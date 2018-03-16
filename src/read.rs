2a98aa74da36689017b066887ee08cd865297210
G. Mantaos <gmantaos@gmail.com>
2018-03-16 11:14:35 +0200

    hid some things

--- src/read.rs
+++ src/read.rs
@@ -137,21 +137,21 @@ impl Read for File
 
 
 #[cfg(target_endian="big")]
-pub fn bytes_to_u16(x: [u8; 2]) -> u16
+fn bytes_to_u16(x: [u8; 2]) -> u16
 {
     ((x[0] as u16) <<  8) +
     ((x[1] as u16) <<  0)
 }
 
 #[cfg(target_endian="little")]
-pub fn bytes_to_u16(x: [u8; 2]) -> u16
+fn bytes_to_u16(x: [u8; 2]) -> u16
 {
     ((x[1] as u16) <<  8) +
     ((x[0] as u16) <<  0)
 }
 
 #[cfg(target_endian="big")]
-pub fn bytes_to_u32(x: [u8; 4]) -> u32
+fn bytes_to_u32(x: [u8; 4]) -> u32
 {
     ((x[0] as u32) << 24) +
     ((x[1] as u32) << 16) +
@@ -160,7 +160,7 @@ pub fn bytes_to_u32(x: [u8; 4]) -> u32
 }
 
 #[cfg(target_endian="little")]
-pub fn bytes_to_u32(x: [u8; 4]) -> u32
+fn bytes_to_u32(x: [u8; 4]) -> u32
 {
     ((x[3] as u32) << 24) +
     ((x[2] as u32) << 16) +
@@ -169,7 +169,7 @@ pub fn bytes_to_u32(x: [u8; 4]) -> u32
 }
 
 #[cfg(target_endian="big")]
-pub fn bytes_to_u64(x: [u8; 8]) -> u64
+fn bytes_to_u64(x: [u8; 8]) -> u64
 {
     ((x[0] as u64) << 56) +
     ((x[1] as u64) << 48) +
@@ -182,7 +182,7 @@ pub fn bytes_to_u64(x: [u8; 8]) -> u64
 }
 
 #[cfg(target_endian="little")]
-pub fn bytes_to_u64(x: [u8; 8]) -> u64
+fn bytes_to_u64(x: [u8; 8]) -> u64
 {
     ((x[7] as u64) << 56) +
     ((x[6] as u64) << 48) +
